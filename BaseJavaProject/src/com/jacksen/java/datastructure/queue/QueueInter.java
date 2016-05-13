86bcf8bdef6efe69db25ea8a1a2c62560c7eaeef
jacksen <yansen19920101@gmail.com>
2016-05-13 19:13:49 +0800

    update queue

---
+++ BaseJavaProject/src/com/jacksen/java/datastructure/queue/QueueInter.java
@@ -0,0 +1,16 @@
+package com.jacksen.java.datastructure.queue;
+
+/**
+ * 队列接口
+ * 
+ * @author jacksen
+ * @date 2016-5-13 下午6:44:48
+ */
+public interface QueueInter<E> {
+	void clear();
+
+	boolean empty();
+
+	int length();
+
+}
---
+++ BaseJavaProject/src/com/jacksen/java/datastructure/queue/Test.java
@@ -0,0 +1,19 @@
+package com.jacksen.java.datastructure.queue;
+
+import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * 
+ * @author jacksen
+ * @date 2016-5-13 下午6:48:11
+ */
+public class Test {
+	public static void main(String[] args) {
+		testQueue();
+	}
+
+	private static void testQueue() {
+		
+	}
+}
