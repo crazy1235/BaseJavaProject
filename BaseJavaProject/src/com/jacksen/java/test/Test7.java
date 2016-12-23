25b30fb024f6078b5209b8a73ba5e37c5ab928a1
jacksen <yansen19920101@gmail.com>
2016-12-23 10:27:36 +0800

    update

---
+++ BaseJavaProject/src/com/jacksen/java/test/Test7.java
@@ -0,0 +1,27 @@
+package com.jacksen.java.test;
+
+public class Test7 {
+
+}
+
+
+
+interface A{
+	void B();
+}
+
+
+abstract class C implements A{
+
+
+}
+
+class D extends C{
+
+	@Override
+	public void B() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
