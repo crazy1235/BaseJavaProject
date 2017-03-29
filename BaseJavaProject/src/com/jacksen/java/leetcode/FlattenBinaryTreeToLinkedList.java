7b806bc7622249f8571b0fc765af207f15a9ee8a
jacksen <951532416@qq.com>
2017-03-29 23:22:23 +0800

    update

---
+++ BaseJavaProject/src/com/jacksen/java/leetcode/FlattenBinaryTreeToLinkedList.java
@@ -0,0 +1,14 @@
+package com.jacksen.java.leetcode;
+
+/**
+ * https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+ * 
+ * @author jacksen
+ * 
+ */
+public class FlattenBinaryTreeToLinkedList {
+
+	public static void main(String[] args) {
+
+	}
+}
