3a7b29178720bbbaf5ecff4165d3ee9ca3dfff65
jacksen <yansen19920101@gmail.com>
2017-04-18 14:55:18 +0800

    update

--- BaseJavaProject/src/com/jacksen/java/test/Test10.java
+++ BaseJavaProject/src/com/jacksen/java/test/Test10.java
@@ -1,11 +1,22 @@
 package com.jacksen.java.test;
 
+import java.io.File;
+
 public class Test10 {
 
 	public static void main(String[] args) {
 
 		int key = 2;
 		System.out.println(getValue(key));
+		
+		System.out.println(10f/9f);
+		
+		File file= new File("");
+		String path = file.getPath();
+		System.out.println(path);
+		file = new File(file, "1234");
+		path = file.getPath();
+		System.out.println(path);
 	}
 
 	public static String getValue(int key) {
