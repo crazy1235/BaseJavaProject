755bb159e79adc044d911251ae3773a1125507c5
jacksen <yansen19920101@gmail.com>
2017-09-05 17:21:32 +0800

    update

--- BaseJavaProject/src/com/jacksen/java/test/Test15.java
+++ BaseJavaProject/src/com/jacksen/java/test/Test15.java
@@ -13,13 +13,43 @@ public class Test15 {
 		System.out.println(1 & (1 << 11));
 
 		System.out.println((1 & ((1 << 3) | (1 << 4))));
-		
+
 		System.out.println((1 & ((1 << 2) | (1 << 5))));
-		
+
 		System.out.println(1 & (1 << 7));
-		
+
 		System.out.println(1 & (1 << 1));
-		
+
 		System.out.println(1 & (1 << 10));
+
+		System.out.println(0x01000000);
+
+		System.out.println(Integer.toBinaryString(0x01000000));
+		System.out.println(Integer.toBinaryString(16777216));
+
+		System.out.println(0xff000000);
+
+		System.out.println(1 << 2);
+
+		System.out.println(0x3 << 30);
+
+		System.out.println(0xff000000 >> 16);
+
+		System.out.println(0xff000000);
+
+		System.out.println("---------------------");
+
+		long a = 1497433752039L;
+		long b = 1497433620738L;
+		float c = 10000f;
+		System.out.println(b + c);
+
+		String x = new String("123");
+		change(x);
+		System.out.println(x);
+	}
+
+	public static void change(String x) {
+		x = "abc";
 	}
 }
--- BaseJavaProject/src/com/jacksen/java/test/TestMap.java
+++ BaseJavaProject/src/com/jacksen/java/test/TestMap.java
@@ -1,6 +1,7 @@
 package com.jacksen.java.test;
 
 import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * 测试Map的子类
@@ -23,7 +24,7 @@ public class TestMap {
 		System.out.println(hashMap.size());
 
 		hashMap.put("2", "b");
-
+		
 		System.out.println(hashMap.size());
 
 		hashMap.put("2", "bb");
@@ -33,7 +34,19 @@ public class TestMap {
 		hashMap.put(null, "c");
 
 		System.out.println(hashMap.size());
-
+		
+		System.out.println(tableSizeFor(16));
 	}
+	
+	static final int MAXIMUM_CAPACITY = 1 << 30;
+	static final int tableSizeFor(int cap) {
+        int n = cap - 1;
+        n |= n >>> 1;
+        n |= n >>> 2;
+        n |= n >>> 4;
+        n |= n >>> 8;
+        n |= n >>> 16;
+        return (n < 0) ? 1 : (n >= MAXIMUM_CAPACITY) ? MAXIMUM_CAPACITY : n + 1;
+    }
 
 }
